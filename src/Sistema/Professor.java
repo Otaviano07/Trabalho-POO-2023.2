d89f1e7beea56fa130d3859cc4f04ad7694aad86
douglasfnascimento <douglasfnascimento@icloud.com>
2023-10-12 00:30:14 -0300

    feito o esboço da classe Professor

--- src/Sistema/Professor.java
+++ src/Sistema/Professor.java
@@ -1,5 +1,33 @@
 package Sistema;
 
 public class Professor {
+    private int idProfessor;
 
+    public Professor() {
+        this.idProfessor = 0;
+    }
+
+    public int getIdProfessor() {
+        return this.idProfessor;
+    }
+
+    public void setIdProfessor(int idProfessor) {
+        this.idProfessor = idProfessor;
+    }
+
+    public boolean compartilharReserva() {
+        return true;
+    }
+
+    public void aceitarReserva() {
+
+    }
+
+    public void alterarReserva() {
+
+    }
+
+    public void receberReserva() {
+
+    }
 }
