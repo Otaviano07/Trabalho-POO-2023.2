3058900e5e299bd407ac9db4fb69d94159b8d353
Otaviano Rodrigues da Silva <42277805+Otaviano07@users.noreply.github.com>
2023-12-04 13:41:31 -0300

    Atualização de codigo.

---
+++ src/main/java/sistema/view/ViewAdministrador.java
@@ -0,0 +1,11 @@
+package main.java.sistema.view;
+
+public class ViewAdministrador {
+    public void mostrarMenuAdministrador() {
+        System.out.println("1 - Gerenciar espaços");
+        System.out.println("2 - Gerenciar equipamentos");
+        System.out.println("3 - Gerenciar reservas");
+        System.out.println("3 - Gerenciar usuários");
+        System.out.println("4 - Voltar");
+    }
+}
---
+++ src/main/java/sistema/view/ViewAluno.java
@@ -0,0 +1,9 @@
+package main.java.sistema.view;
+
+public class ViewAluno {
+
+        public void mostrarMenuAluno() {
+            System.out.println("1 - Gerenciar reservas");
+            System.out.println("2 - Voltar");
+        }
+}
